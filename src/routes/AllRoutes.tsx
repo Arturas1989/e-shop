70a603d71185c0df8e06555821f607e82d4effaf
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2024-02-14 18:33:25 +0200

    #8 AllRoutes component

---
+++ src/routes/AllRoutes.tsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import { Home, PageNotFound } from "../pages";
+
+export const AllRoutes = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='*' element={<PageNotFound />} />
+    </Routes>
+  )
+};
+
