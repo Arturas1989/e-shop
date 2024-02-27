0385b00d48bbc6d0f32a27b437e6639b4684848e
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2024-02-27 21:17:28 +0200

    #23 FeaturedProducts

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,11 @@
+import { FeaturedProducts } from "./components/FeaturedProducts/FeaturedProducts";
 import { Hero } from "./components/Hero/Hero";
 
 export const Home = () => {
   return (
-    <main data-testid='home'>
+    <main data-testid='home' className="home">
       <Hero />
+      <FeaturedProducts />
     </main>
   )
 };
