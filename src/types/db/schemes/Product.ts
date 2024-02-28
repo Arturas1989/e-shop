019a3066f465c0c9d03461c0724eed35a8e7e394
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2024-02-28 20:25:05 +0200

    #25 collection types

---
+++ src/types/db/schemes/Product.ts
@@ -0,0 +1,11 @@
+import { RatingNum } from '../../RatingNum';
+
+export type Product = {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  rating: RatingNum;
+  best_seller: boolean;
+  img_name: string;
+};
