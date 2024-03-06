7e133347ea499b65b02963efe324b7877626ac89
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2024-03-06 17:24:22 +0200

    fixed productCard tests

--- src/__tests__/components/productCard.test.tsx
+++ src/__tests__/components/productCard.test.tsx
@@ -1,13 +1,22 @@
 import renderer from 'react-test-renderer';
 import { BrowserRouter } from 'react-router-dom';
 import { ProductCard } from '../../components/ProductCard/ProductCard';
+import { Product } from '../../types';
 
-describe('footer components renders', () => {
+describe('ProductCard components renders', () => {
   test('renders correctly', () => {
+    const product: Product = {
+      name: 'headphones',
+      description: 'this is headphones',
+      price: 99,
+      rating: 5,
+      best_seller: false,
+      img_name: 'headphones.jpg'
+    }
     const tree = renderer
       .create(
         <BrowserRouter>
-          <ProductCard />
+          <ProductCard product={product} />
         </BrowserRouter>
       )
       .toJSON();
