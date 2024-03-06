79e24e8d7bced0366cb142caac93bec13a4aed1b
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2024-03-06 19:38:20 +0200

    #27 toast implementation

--- src/hooks/useData.tsx
+++ src/hooks/useData.tsx
@@ -1,18 +1,23 @@
-import { DocumentData, collection, getDocs } from "firebase/firestore";
-import { useEffect, useState } from "react";
-import { db } from "../db/firebase";
-import type { Collection } from "../types/db/Collection";
+import { DocumentData, collection, getDocs } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { db } from '../db/firebase';
+import { credentialsNotSet } from '../utils/helpers';
+import type { Collection } from '../types';
+
 
 export const useData = <T extends DocumentData>(colName: Collection) => {
   const [colData, setColData] = useState<T[] | null>(null);
+  const [error, setError] = useState('');
+
   useEffect(() => {
-    const colRef = collection(db, 'products')
-    getDocs(colRef).then(fireData => {
-      const data = fireData.docs.map(doc => doc.data() as T);
-      console.log(data)
-      setColData(fireData.docs.map(doc => doc.data() as T));
-      return;
-    })
-  }, [])
-  return colData;
-}
+    if (credentialsNotSet(db.app.options)) {
+      setError(`${colName} failed to fetch`);
+    } else {
+      const colRef = collection(db, colName);
+      getDocs(colRef).then((fireData) => {
+        setColData(fireData.docs.map((doc) => doc.data() as T));
+      });
+    }
+  }, [colName]);
+  return [colData, error] as [T[] | null, string];
+};
