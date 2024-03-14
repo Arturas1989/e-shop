a6f4c1a9c73a96b31ec2aaea2bbd3fdce1c4db46
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2024-03-14 20:54:54 +0200

    #42 added Faq component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { Hero, FeaturedProducts, Testimonials } from "./index";
+import { Hero, FeaturedProducts, Testimonials, Faq } from "./index";
 
 export const Home = () => {
   return (
@@ -6,6 +6,7 @@ export const Home = () => {
       <Hero />
       <FeaturedProducts />
       <Testimonials />
+      <Faq />
     </main>
   )
 };
