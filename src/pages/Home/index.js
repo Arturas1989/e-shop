9906293c2740c3ee4bcc7c329bdfc35a2f1892a6
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2024-03-14 20:55:15 +0200

    #42 Faq export

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,4 @@
 export { Hero } from './components/Hero/Hero';
 export { FeaturedProducts } from './components/FeaturedProducts/FeaturedProducts';
 export { Testimonials } from './components/Testimonials/Testimonials';
+export { Faq } from './components/Faq/Faq';
