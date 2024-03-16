feaaf7765a5a783cb2f22032f79ae40a9cd7adb6
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2024-03-16 17:14:41 +0200

    logout feature

--- src/components/Header/Menu/LoginMenu/LoginLink.tsx
+++ src/components/Header/Menu/LoginMenu/LoginLink.tsx
@@ -1,14 +1,30 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import { getAuth, signOut } from 'firebase/auth';
+import { useLogin } from '../../../../hooks/useLogin';
 
-type LoginProps = {
-  name?: string;
-};
 
 export const LoginLink = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
-  return (
-    isLoggedIn ? <div className="menu-item" onClick={() => setIsLoggedIn(false)}>Logout</div> 
-        : <Link to='/login' className="menu-item">Login</Link>
-  )
+  const isLoggedIn = useLogin();
+  
+  const logout = () => {
+    const auth = getAuth();
+      signOut(auth).then(() => {
+        // Sign-out successful.
+      }).catch((error) => {
+        // An error happened.
+      });
+  }
+  
+  return isLoggedIn ? (
+    <div
+      className="menu-item"
+      onClick={() => logout()}
+    >
+      Logout
+    </div>
+  ) : (
+    <Link to="/login" className="menu-item">
+      Login
+    </Link>
+  );
 };
