efb1b94682019debf12a91ceb5f9a1199a97d9e0
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2024-03-16 17:19:28 +0200

    #46 Register route

--- src/routes/AllRoutes.tsx
+++ src/routes/AllRoutes.tsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from "react-router-dom";
-import { Home, PageNotFound } from "../pages";
+import { Home, PageNotFound, Register } from "../pages";
 
 export const AllRoutes = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='register' element={<Register />} />
       <Route path='*' element={<PageNotFound />} />
     </Routes>
   )
