545dd03b3eeb05dd93f69fcc0bb153281dc84256
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2024-03-28 19:13:02 +0200

    restructured

--- src/__tests__/pages/Auth/Register/register.test.tsx
+++ src/__tests__/pages/Auth/Register/register.test.tsx
@@ -1,6 +1,6 @@
 import renderer from 'react-test-renderer';
 import { BrowserRouter } from 'react-router-dom';
-import { Register } from '../../../pages';
+import { Register } from '../../../../pages';
 
 describe('Register page test', () => {
   test('render test', () => {
