af0d4e315dc7b4b74dade966c81fa4fa31807d66
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2024-03-29 20:10:27 +0200

    #56 login feature

--- src/pages/Auth/Login/Login.tsx
+++ src/pages/Auth/Login/Login.tsx
@@ -1,15 +1,22 @@
+import { useNavigate } from 'react-router-dom';
 import { FormField, Heading, SecondaryButton } from '../../../components';
+import { Auth } from '../../../services/Auth';
+import { type FormFields } from '../../../types';
 
-type LoginProps = {
-  name?: string;
-};
-
-export const Login = ({ name }: LoginProps) => {
+export const Login = () => {
+  const navigate = useNavigate();
+  const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = e.target as EventTarget & Omit<FormFields, 'name'>;
+    const { email, password } = form;
+    const auth = new Auth(email.value, password.value, navigate);
+    auth.login();
+  }
   return (
     <main className="main login">
       <section className="main-section">
         <Heading>Login</Heading>
-        <form className="main-form">
+        <form className="main-form" onSubmit={(e) => handleLogin(e)}>
           <FormField
             type="email"
             htmlFor="email"
