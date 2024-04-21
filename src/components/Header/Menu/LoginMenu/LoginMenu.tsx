324e14d3f204d9a495060965114e00899c793853
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2024-04-21 15:45:26 +0300

    #60 display name and email in login menu

--- src/components/Header/Menu/LoginMenu/LoginMenu.tsx
+++ src/components/Header/Menu/LoginMenu/LoginMenu.tsx
@@ -1,5 +1,7 @@
-import { Link } from "react-router-dom";
-import { LoginLink } from "./LoginLink";
+import { Link } from 'react-router-dom';
+import { LoginLink } from './LoginLink';
+import { checkAuth } from '../../../../hooks/useLogin';
+import { type User } from 'firebase/auth';
 
 type LoginMenuProps = {
   isVisible: boolean;
@@ -7,16 +9,31 @@ type LoginMenuProps = {
 };
 
 export const LoginMenu = ({ isVisible, isLoggedIn }: LoginMenuProps) => {
-  return (
-    isVisible ?
+  const auth = checkAuth();
+  let { currentUser } = auth;
+  return isVisible ? (
     <nav data-testid="login-menu" className="login-menu">
       <div className="container">
-        <Link to='/products' className="menu-item">All products</Link>
+        {currentUser && (
+          <>
+            <p>
+              name: <span>{currentUser.displayName}</span>
+            </p>
+            <p>
+              email: <span>{currentUser.email}</span>
+            </p>
+          </>
+        )}
+        <Link to="/products" className="menu-item">
+          All products
+        </Link>
         <LoginLink />
-        {!isLoggedIn && <Link to='/register' className="menu-item">Register</Link>}
+        {!isLoggedIn && (
+          <Link to="/register" className="menu-item">
+            Register
+          </Link>
+        )}
       </div>
     </nav>
-    :
-    null
-  )
+  ) : null;
 };
