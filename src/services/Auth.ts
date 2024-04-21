eb4a51b4a29f82c79dd8d0adb25966a36c967761
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2024-04-21 15:46:04 +0300

    #60 update user displayName

--- src/services/Auth.ts
+++ src/services/Auth.ts
@@ -1,9 +1,14 @@
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import {
+  getAuth,
+  updateProfile,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  type User,
+} from 'firebase/auth';
 import { doc, setDoc } from 'firebase/firestore';
 import { db } from '../db/firebase';
 import { toast } from 'react-toastify';
 import { type NavigateFunction } from 'react-router-dom';
-
 
 export class Auth {
   private email;
@@ -11,40 +16,55 @@ export class Auth {
   private navigate;
   private auth;
 
-  constructor(email: string, password: string, navigate: NavigateFunction){
+  constructor(email: string, password: string, navigate: NavigateFunction) {
     this.email = email;
     this.password = password;
-    this.navigate = navigate
+    this.navigate = navigate;
     this.auth = getAuth();
   }
 
- register(name: string){
+  register(name: string) {
     createUserWithEmailAndPassword(this.auth, this.email, this.password)
       .then((userCredential) => {
         const user = userCredential.user;
-        setDoc(doc(db, 'users', user.uid), {
-          name,
-          email: this.email,
-        });
+        this.updateUser(name, user);
+        
         this.navigate('/');
       })
       .catch((error) => {
         const emailExist =
           error.message.indexOf('auth/email-already-in-use') !== -1;
         if (emailExist) toast.error('email already exists');
-        console.clear()
+        console.clear();
       });
   }
 
- login(){
+  login() {
     signInWithEmailAndPassword(this.auth, this.email, this.password)
-    .then(() => {
-      this.navigate('/');
+      .then(() => {
+        this.navigate('/');
+      })
+      .catch((error) => {
+        if (error.code === 'auth/invalid-login-credentials')
+          toast.error('incorect email or password');
+        if (error.code === 'auth/too-many-requests')
+          toast.error('too many requests, please wait');
+        console.clear();
+      });
+  }
+
+  updateUser(name: string, user: User) {
+    updateProfile(user, {
+      displayName: name,
     })
-    .catch((error) => {
-      if(error.code === 'auth/invalid-login-credentials') toast.error('incorect email or password');
-      if(error.code === 'auth/too-many-requests') toast.error('too many requests, please wait');
-      console.clear()
-    });
+      .then(() => {})
+      .catch((error) => {
+        toast.error(error);
+        console.clear();
+      });
+      setDoc(doc(db, 'users', user.uid), {
+        name,
+        email: this.email,
+      });
   }
 }
