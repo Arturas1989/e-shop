98fe6505d19aad218a9cebcdc759a6d2edf02871
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2024-04-25 18:20:30 +0300

    #65 removed toast error, added loading

--- src/pages/Home/components/FeaturedProducts/FeaturedProducts.tsx
+++ src/pages/Home/components/FeaturedProducts/FeaturedProducts.tsx
@@ -1,25 +1,19 @@
-import { useEffect } from 'react';
 import { Products } from '../../../../components/Products/Products';
 import { useData } from '../../../../hooks/useData';
 import { DataInfo, Product } from '../../../../types';
-import { toast } from 'react-toastify';
-import { Heading } from '../../../../components';
+import { Heading, Loading } from '../../../../components';
 
 export const FeaturedProducts = () => {
   const dataInfo: DataInfo = {
     collectionName: 'products',
     onlyFeatured: true
   }
-  const [products, error] = useData<Product>(dataInfo);
-
-  useEffect(() => {
-    if (error) toast.error(error);
-  },[error]);
+  const [products, isLoaded] = useData<Product>(dataInfo);
 
   return (
     <section className="feature">
       <Heading>Featured products</Heading>
-      <Products products={products} />
+      {isLoaded ? <Products products={products}/> : <Loading />}
     </section>
   );
 };
