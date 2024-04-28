e07272ace159601d3af49d123e961b388f0b7010
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2024-04-28 15:48:52 +0300

    #68 input group component

---
+++ src/pages/Products/components/Filter/InputGroup.tsx
@@ -0,0 +1,36 @@
+import { HTMLInputTypeAttribute, type ReactNode } from 'react';
+import { FilterFields } from '../../../../types';
+
+type InputGroupProps = {
+  className: string;
+  type: HTMLInputTypeAttribute;
+  name: keyof FilterFields;
+  id: string;
+  children: ReactNode;
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  fields: FilterFields;
+};
+
+export const InputGroup = ({
+  handleChange,
+  fields,
+  className,
+  type,
+  name,
+  id,
+  children,
+}: InputGroupProps) => {
+  return (
+    <div className={className}>
+      <input
+        data-testid={name}
+        onChange={(e) => handleChange(e)}
+        type={type}
+        name={name}
+        id={id}
+        checked={fields[name] === id}
+      />
+      <label htmlFor={name}>{children}</label>
+    </div>
+  );
+};
