d3faeeb9be78f6d3a4dee8a7c9030a4735afe415
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2024-05-01 18:57:54 +0300

    #74 protected products page

--- src/routes/AllRoutes.tsx
+++ src/routes/AllRoutes.tsx
@@ -1,15 +1,22 @@
-import { Routes, Route } from "react-router-dom";
-import { Home, Login, PageNotFound, ProductsPage, Register } from "../pages";
+import { Routes, Route } from 'react-router-dom';
+import { Home, Login, PageNotFound, ProductsPage, Register } from '../pages';
+import { ProtectedRoute } from './ProtectedRoute';
 
 export const AllRoutes = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='products' element={<ProductsPage />} />
-      <Route path='register' element={<Register />} />
-      <Route path='login' element={<Login />} />
-      <Route path='*' element={<PageNotFound />} />
+      <Route path="/" element={<Home />} />
+      <Route
+        path="/products"
+        element={
+          <ProtectedRoute redirect="/products">
+            <ProductsPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="*" element={<PageNotFound />} />
     </Routes>
-  )
+  );
 };
-
