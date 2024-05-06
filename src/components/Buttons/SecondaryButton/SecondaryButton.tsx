4afa5875e5b63b7c59fac43ec1d36b780caffb86
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2024-05-06 19:58:37 +0300

    #82 on click add to cart

--- src/components/Buttons/SecondaryButton/SecondaryButton.tsx
+++ src/components/Buttons/SecondaryButton/SecondaryButton.tsx
@@ -1,12 +1,25 @@
 import { type ReactNode } from "react";
+import { useCartContext } from "../../../contexts/cartContext";
+import { Cart } from "../../../services/Cart";
 
 type SecondaryButtonProps = {
+  id?: string;
   children: ReactNode;
 };
 
-export const SecondaryButton = ({ children }: SecondaryButtonProps) => {
+export const SecondaryButton = ({ id, children }: SecondaryButtonProps) => {
+  const {cart, setCart} = useCartContext()!;
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const target = e.currentTarget as EventTarget & {id: string};
+    const addToCart = async(cart: Cart, id: string, quantity: number) => {
+      const newCart = cart.makeCopy();
+      await newCart.addToCart(id, quantity);
+      setCart(newCart);
+    }
+    if(cart) addToCart(cart, target.id, 1)
+  }
   return (
-    <button className="secondary-button">
+    <button id={id} onClick={(e) => handleClick(e)} className="secondary-button">
       {children}
     </button>
   )
