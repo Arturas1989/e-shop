361e98658e78c236c6f5702bc32c27bf38385503
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2024-06-15 20:53:48 +0300

    getting products from cart context

--- src/pages/Cart/components/CartProducts.tsx
+++ src/pages/Cart/components/CartProducts.tsx
@@ -1,15 +1,13 @@
 import { Loading } from '../../../components';
 import { useCartContext } from '../../../contexts/cartContext';
-import { useCartProducts } from '../../../hooks';
 import { CartContainer } from './CartContainer';
 
 export const CartProducts = () => {
-  const { cart } = useCartContext()!;
-  const [isLoading, products] = useCartProducts(cart);
+  const { isCartLoading, products } = useCartContext()!;
   
   return (
     <section className="cards">
-      {isLoading ? <Loading /> : <CartContainer products={products}/>}
+      {isCartLoading ? <Loading /> : <CartContainer products={products}/>}
     </section>
   );
 };
