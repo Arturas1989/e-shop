10c7e4fbc974ebbb07c2e5a283b6c32e8075f62b
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2024-06-16 16:24:02 +0300

    changed data-testid

--- src/components/SearchBar/SearchIcon.tsx
+++ src/components/SearchBar/SearchIcon.tsx
@@ -10,7 +10,7 @@ export const SearchIcon = ({ setMenuOnLocation }: SearchIconProps) => {
   const className = `bi bi-search ${location.pathname === '/cart' ? 'disabled' : ''}`
   console.log(className)
   return (
-    <i data-testid="search-icon2" className={className} onClick={() => setMenuOnLocation!('searchVisible')}></i>
+    <i data-testid="search-icon" className={className} onClick={() => setMenuOnLocation!('searchVisible')}></i>
   )
 };
 
